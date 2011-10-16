243074cefc18bb0044163f9e20e18cf5c262242e
Jaroslav Vazny <jaroslav.vazny@gmai.com>
2011-10-17 00:16:28 +0200

    math demonstration using MathJax.js

--- uvod.php
+++ uvod.php
@@ -1,4 +1,28 @@
-<h2>Úvod</h2>
+<!DOCTYPE html>
+<html>
+<head>
+<title>MathJax TeX Test Page</title>
+<script type="text/x-mathjax-config">
+  MathJax.Hub.Config({tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]}});
+</script>
+<script type="text/javascript"
+  src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML">
+</script>
+</head>
+<body>
+
+
+<h2>
+<div style="font-size: 100%;">
+\[
+\mathbf{B}^n = \{ (x_1, x_2, \dots, x_n)\in\mathbb{R}^n \mid x_1^2 + x_2^2 + \cdots + x_n^2 < 1 \}.
+\]
+</div>
+</h2>
+
+<object data="Isaac_Newton_signature.svg" type="image/svg+xml"></object>
+
+<img src="200px-Isaac_Newton_signature.svg.png" alt="Bitmapový obrázek" />
 
 <p>Vítejte na stránkách Kurzu klasické mechaniky v úlohách a testech pro e-learning, které vznikly v rámci projektu <a href="http://www.frvs.cz/">FRVŠ</a> 1592/2009.</p>
 
@@ -8,6 +32,8 @@
 
 <h3>Co na stránkách naleznete?</h3>
 
+<p>$R_{ab} - {\textstyle 1 \over 2}R\,g_{ab} = {8 \pi G \over c^4} T_{ab}.$</p>
+
 <ul>
 <li>Informace <a href="index.php?web=projekt"><b>O projektu</b></a> spolu s kontakty na řešitelku, spoluřešitelku a spolupracovníky.</li>
 <li><a href="index.php?web=nastrahy"><b>Nástrahy fyziky</b></a> (deset krátkých relativně nezávislých učebních textů zaměřených na problematická místa klasické mechaniky <br>a molekulové fyziky uvozených vstupním přehledem tematiky).</li>
@@ -16,3 +42,6 @@
 <li><a href="index.php?web=experimenty"><b>Demonstrační experimenty</b></a> (videozáznamy více i méně tradičních demonstračních experimentů spolu s výkladem či jednoduchými kontrolními úkoly).</li>
 <li><a href="index.php?web=odkazy"><b>Odkazy</b></a> na vybrané internetové stránky související <br>s problematikou projektu.</li>
 </ul>
+
+</body>
+</html>
